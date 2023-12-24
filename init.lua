fd513d4be14d63abd26307fd04ccc260be7c28c6
Yisus <jesusgameroom@gmail.com>
2023-12-24 19:08:26 -0400

    settings for nvim tree

--- init.lua
+++ init.lua
@@ -5,21 +5,17 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    hijack_cursor = true,
+    renderer = {
+        indent_markers = {
+            enable = true
+        }
+    },
+    view = {
+        width = 15
+    }
 })
 
 require 'yisus'
