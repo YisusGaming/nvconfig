fb67cd8de0630e9804421709513a54acf0f7299a
Yisus <jesusgameroom@gmail.com>
2023-12-24 14:13:10 -0400

    more telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>di', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
