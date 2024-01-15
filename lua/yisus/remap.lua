ab20885bc9c60c4190c1345e3e1f99dec54c2c9b
Yisus <jesusgameroom@gmail.com>
2024-01-15 17:34:52 -0400

    added remap for :q

--- lua/yisus/remap.lua
+++ lua/yisus/remap.lua
@@ -5,6 +5,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew)
 vim.keymap.set("n", "<leader>x", vim.cmd.tabclose)
+vim.keymap.set("n", "<leader>zx", vim.cmd.q)
 vim.keymap.set("n", "<Tab>", vim.cmd.tabnext);
 vim.keymap.set("n", "<S-Tab>", vim.cmd.tabprevious)
 
